88eb79d37e1fab0777d7c71be98d7f25bdaec34e
manuelgd11 <manuelgd011@gmail.com>
2021-08-25 14:31:19 -0500

    Creando el repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { BodyComponent } from './componentes/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
